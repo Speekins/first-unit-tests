686612b3ff92bee3d7511dfbdde0fc4ad6d2830e
Spencer Haka <spencer.haka@gmail.com>
2022-08-25 08:45:36 -0600

    completed testing warm up on function tests

--- functions.js
+++ functions.js
@@ -1,9 +1,27 @@
 // functions.js
-function addTwoNumbers() {
+function addTwoNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+function sayHello(name) {
+    if (name === "Will") {
+        return `No more testing Will!`
+    }
+    return `Hi there ${name}!`
+}
+
+function buildCar(color, type) {
+    if (!color && !type) {
+        return {};
+    }
+    return {
+        color: color,
+        type: type
+    }
 }
 
 module.exports = {
     addTwoNumbers,
-    // sayHello,
-    // buildCar
+    sayHello,
+    buildCar
 }
